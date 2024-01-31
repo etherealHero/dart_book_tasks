a39f1e5842dfd937d19d19101481db89ca853339
etherealhero <gutcan4312@gmail.com>
2024-01-31 23:09:21 +1000

    chapter 1 task 1

--- chapter_1/bin/chapter_1.dart
+++ chapter_1/bin/chapter_1.dart
@@ -1,5 +1,5 @@
 import 'package:chapter_1/chapter_1.dart' as chapter_1;
 
 void main(List<String> arguments) {
-  print('Hello world: ${chapter_1.calculate()}!');
+  chapter_1.start(arguments);
 }
--- chapter_1/lib/chapter_1.dart
+++ chapter_1/lib/chapter_1.dart
@@ -1,3 +1,27 @@
-int calculate() {
-  return 6 * 7;
+/*
+1. напишите приложение, где пользователь вводит строку и букву,
+наличие которой предстоит проверить в введенной строке. выведите в
+терминал полученный результат в терминал, а также индекс последнего
+вхождения буквы в строку.
+*/
+
+import 'dart:io';
+
+void start(List<String> arguments) {
+  stdout.writeln("Введите строку, затем букву, которую необходимо найти: ");
+
+  String? str = stdin.readLineSync();
+  String? letter = stdin.readLineSync();
+
+  if (str == null || letter == null || str.isEmpty || letter.length != 1) {
+    return stdout.writeln("Неверные входные данные");
+  }
+
+  print("Строка: $str");
+  print("Буква: $letter");
+  print(str.contains(letter)
+      ? "'$letter' содержится в '$str'"
+      : "'$letter' не содержится в '$str'");
+  print(
+      "индекс последнего вхождения буквы в строку: ${str.lastIndexOf(letter)}");
 }
