9a29deb76222ce21f8056955216e21a29d3d786a
maximgutsan <khf187@khf.ru>
2024-02-29 18:51:14 +1000

    test refactor

--- chapter_3/test/chapter_3_test.dart
+++ chapter_3/test/chapter_3_test.dart
@@ -11,26 +11,28 @@ void main() {
       ['run', '${Directory.current.path}\\bin\\chapter_3.dart'],
     );
 
-    var step = 0;
-    var workflowCase = [
-      ("Номер Лабораторной работы 4, 5, или 6: ", "4"),
-      ("Введите номер задания: ", "1"),
-      ("Введите целочисленный список: ", "11 28 -10"),
-      ("Максимальное число: 28", ""),
-    ];
+    testCase() sync* {
+      yield ("Номер Лабораторной работы 4, 5, или 6: ", "4");
+      yield ("Введите номер задания: ", "1");
+      yield ("Введите целочисленный список: ", "11 28 -10");
+      yield ("Максимальное число: 28", null);
+    }
+
+    var io = testCase().iterator;
 
     process.stdout.transform(utf8.decoder).transform(LineSplitter()).listen(
-      (String lineOutput) {
-        var (String expectedOutput, String inputLine) = workflowCase[step];
-
-        expect(lineOutput, expectedOutput);
-
-        if (step < workflowCase.length) process.stdin.writeln(inputLine);
-        step++;
+      (String output) {
+        if (io.moveNext()) {
+          var (String expectedOutput, String? input) = io.current;
+          expect(output, expectedOutput);
+          process.stdin.writeln(input);
+        } else {
+          process.kill();
+        }
       },
     );
 
     var exitCode = await process.exitCode;
-    expect(exitCode, 0);
+    expect(exitCode, 0, reason: "Ожидалось завершение программы");
   });
 }
