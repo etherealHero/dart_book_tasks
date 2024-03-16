527123755f6271051a73ee72ece7414f98b83eaa
etherealhero <gutcan4312@gmail.com>
2024-03-16 16:21:17 +1000

    ch 4 lab 7 fix missing test 1

--- chapter_4/lib/src/lab_7/task_1/resident.dart
+++ chapter_4/lib/src/lab_7/task_1/resident.dart
@@ -1,7 +1,14 @@
 class Person {
-  String name;
+  final String name;
 
-  Person(this.name);
+  static final _personMap = <String, Person>{};
+
+  Person._fromName(this.name);
+
+  factory Person(String name) {
+    String ident = name.toLowerCase();
+    return _personMap.putIfAbsent(ident, () => Person._fromName(name));
+  }
 
   @override
   String toString() => name;
