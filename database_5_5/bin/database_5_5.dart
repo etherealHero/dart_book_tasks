edb82ea7247af5d0e2bfde85383d7124ca557267
etherealhero <gutcan4312@gmail.com>
2024-03-17 20:27:15 +1000

    ch 5_5 database

--- database_5_5/bin/database_5_5.dart
+++ database_5_5/bin/database_5_5.dart
@@ -1,5 +1,12 @@
-import 'package:database_5_5/database_5_5.dart' as database_5_5;
+import 'package:database_5_5/database_5_5.dart';
+
+import 'menu.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${database_5_5.calculate()}!');
+  var db = Database(
+    pathToSuaiDB: 'bin\\suai.txt',
+    pathToUneconDB: 'bin\\unecon.txt',
+  );
+  var menu = Menu(db);
+  menu.loop();
 }
--- database_5_5/test/database_5_5_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:database_5_5/database_5_5.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
